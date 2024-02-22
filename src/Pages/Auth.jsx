6c202b4b669fa0917c611fbf2c5aa83ac81f8d5a
Shamilxmhd <shamilxmuhd@gmail.com>
2024-02-22 13:17:14 +0530

    9th

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -6,18 +6,16 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { loginAPI, registerAPI } from '../Services/allAPIs';
 import { tokenAuthContext } from '../ContextAPI/TokenAuth';
-import PulseLoader from "react-spinners/PulseLoader";
+import ClipLoader from "react-spinners/ClipLoader";
 import HashLoader from "react-spinners/HashLoader";
 
 function Auth({ insideRegister }) {
-  const [loading, setLoading] = useState(false)
-
-
-
+  const [isSubmit, setIsSubmit] = useState(false);
   const { isAutherised, setIsAutherised } = useContext(tokenAuthContext)
-
+  const [registerLoading, setRegisterLoading] = useState(false)
+  const [loginLoading, setLoginLoading] = useState(false)
   const navigate = useNavigate()
-
+  const [formErrors, setFormErrors] = useState({});
   const [userData, setUserData] = useState({
     username: '', email: '', password: ''
   })
@@ -27,21 +25,39 @@ function Auth({ insideRegister }) {
   const handleRegister = async (e) => {
     e.preventDefault()
     console.log(userData);
+
     const { username, email, password } = userData
+    const emailErrors = validate(email);
+    if (emailErrors) {
+      setFormErrors({ email: emailErrors });
+      return;
+    }
+    if (password.length < 4) {
+      setFormErrors({ password: "*Password must be at least 4 characters long" });
+      return;
+    }
+
+
     if (!username || !email || !password) {
       toast.info('Please fill the form completely!!!')
+
     } else {
+
       // api call
       try {
         const result = await registerAPI(userData)
         console.log(result);
+        setFormErrors(validate(userData));
+        setIsSubmit(true);
         if (result.status == 200) {
+
           toast.success(`${result.data.username} has registered successfully`)
           setUserData({ username: '', email: '', password: '' })
-          setLoading(true)
+          setRegisterLoading(true)
+
           setTimeout(() => {
             navigate('/login')
-            setLoading(false)
+            setRegisterLoading(false)
           }, 2000);
         } else {
           toast.warning(result.response.data);
@@ -57,7 +73,23 @@ function Auth({ insideRegister }) {
   const handleLogin = async (e) => {
     e.preventDefault()
     console.log(userData);
+
+    setFormErrors(validate(userData));
+    setIsSubmit(true);
+
     const { email, password } = userData
+    const emailErrors = validate(email);
+    if (emailErrors) {
+      setFormErrors({ email: emailErrors });
+      return;
+
+    }
+    if (password.length < 4) {
+      setFormErrors({ password: "*Password must be at least 4 characters long" });
+      return;
+    }
+
+
     if (!email || !password) {
       toast.info('Please fill the form completely!!!')
     } else {
@@ -71,13 +103,13 @@ function Auth({ insideRegister }) {
           sessionStorage.setItem('userDetails', JSON.stringify(result.data.existingUser))
           sessionStorage.setItem('email', result.data.existingUser.email)
           setIsAutherised(true)
-          setLoading(true)
+          setLoginLoading(true)
           setTimeout(() => {
-            setLoading(false)
+            setLoginLoading(false)
             setUserData({ email: '', password: '' })
             navigate('/cards')
 
-          }, 2000);
+          }, 2500);
         } else {
           toast.warning(result.response.data)
         }
@@ -93,77 +125,82 @@ function Auth({ insideRegister }) {
     setShow(!Show)
   }
 
+
+  const validate = (email) => {
+
+    if (!email) {
+      errors.email = "Email is required!";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(email)) {
+      return "*Invalid email format!";
+    }
+    return null;
+  };
+
+
+
   return (
     <div className='d-flex justify-content-center align-items-center' style={{ width: '100%', height: '100vh', backgroundColor: '' }}>
-      <div className='container w-75' >
-        <div className="card  shadow  " style={{ background: 'transparent' }}>
-          <div className="row align-items-center">
-            <div className="col-lg-6">
-              <img className='rounded shadow' style={{ width: '100%', height: '638px' }} src={SignUpImage} alt="no image" />
-            </div>
-            <div className="col-lg-6 p-5">
-              <div className="d-flex flex-column align-items-center ">
-                <Form className='w-100'>
-                  <div className='text-center'>
-                    <span style={{ fontWeight: '700', fontSize: '35px', color: 'black' }}>Get Started</span>
-                    {insideRegister ? <p>Already have an Account?Click here to <Link style={{ color: 'red', fontWeight: '500' }} to={'/login'}>Login</Link></p> :
-                      <p>New User?Click here to <Link style={{ color: 'red', fontWeight: '500' }} to={'/register'}>SignUp</Link></p>}
-                  </div>
-                  {insideRegister && <Form.Group className="mb-3" controlId="formBasicName">
-                    <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Name</span>
-                    <Form.Control className='rounded' type="text" placeholder="Enter name" onChange={e => setUserData({ ...userData, username: e.target.value })} value={userData.username} />
-                  </Form.Group>}
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Email</span>
-
-                    <Form.Control className='rounded' type="email" placeholder="Enter email" onChange={e => setUserData({ ...userData, email: e.target.value })} value={userData.email} />
-                  </Form.Group>
-                  <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Password</span>
-
-                    <div className='d-flex align-items-center mb-2'>
-                      <Form.Control className='rounded ' type={Show ? "text" : "password"} placeholder="Enter password" onChange={e => setUserData({ ...userData, password: e.target.value })} value={userData.password} />
-                      <label style={{ cursor: 'pointer', marginLeft: '-25px' }} onClick={handleShow} >{Show ? <i class="fa-regular fa-eye"></i> : <i class="fa-regular fa-eye-slash"></i>}</label>
+      {loginLoading ?
+        <HashLoader color={'#ED7117'} loading={loginLoading} size={80} aria-label="Loading Spinner" data-testid="loader" />
+        :
+        <div className='container w-75' >
+          <div className="card  shadow  " style={{ background: 'transparent' }}>
+            <div className="row align-items-center">
+              <div className="col-lg-6">
+                <img className='rounded shadow img-fluid' style={{ width: '100%', height: '669px' }} src={SignUpImage} alt="no image" />
+              </div>
+              <div className="col-lg-6 p-5">
+                <div className="d-flex flex-column align-items-center ">
+                  <Form className='w-100'>
+                    <div className='text-center'>
+                      <span style={{ fontWeight: '700', fontSize: '35px', color: 'black' }}>Get Started</span>
+                      {insideRegister ? <p>Already have an Account?Click here to <Link style={{ color: 'red', fontWeight: '500' }} to={'/login'}>Login</Link></p> :
+                        <p>New User?Click here to <Link style={{ color: 'red', fontWeight: '500' }} to={'/register'}>SignUp</Link></p>}
                     </div>
-                  </Form.Group>
-                  {insideRegister ? <div className="d-grid gap-2">
-                    {loading ? <PulseLoader
-                      color={'#ED7117'}
-                      loading={loading}
+                    {insideRegister && <Form.Group className="mb-3" controlId="formBasicName">
+                      <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Name</span>
+                      <Form.Control className='rounded' type="text" placeholder="Enter name" onChange={e => setUserData({ ...userData, username: e.target.value })} value={userData.username} />
+                    </Form.Group>}
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Email</span>
 
-                      size={30}
-                      aria-label="Loading Spinner"
-                      data-testid="loader"
-                    /> : <Button onClick={handleRegister} className='btn  mb-2'>Sign up</Button>}
-                  </div> :
-                    <div className="d-grid gap-2">
-                      {loading ? <HashLoader
-                        color={'#ED7117'}
-                        loading={loading}
-
-                        size={30}
-                        aria-label="Loading Spinner"
-                        data-testid="loader"
-                      /> : <Button onClick={handleLogin} className='btn  mb-2'>Login</Button>}
+                      <Form.Control className='rounded' type="email" placeholder="Enter email" onChange={e => setUserData({ ...userData, email: e.target.value })} value={userData.email} /> <p className='text-danger fs-6'>{formErrors.email}</p>
+                    </Form.Group>
+                    <Form.Group className="mb-2" controlId="formBasicPassword">
+                      <span style={{ fontWeight: '600', fontSize: '20px' }} className='text-dark'>Password</span>
+                      <div className='d-flex align-items-center mb-2'>
+                        <Form.Control className='rounded ' type={Show ? "text" : "password"} placeholder="Enter password" onChange={e => setUserData({ ...userData, password: e.target.value })} value={userData.password} />
+                        <label style={{ cursor: 'pointer', marginLeft: '-25px' }} onClick={handleShow} >{Show ? <i class="fa-regular fa-eye"></i> : <i class="fa-regular fa-eye-slash"></i>}</label>
+                      </div>
+                      <p className='text-danger fs-6'>{formErrors.password}</p>
+                    </Form.Group>
+                    {insideRegister ? <div className="d-grid gap-2">
+                      {registerLoading ?
+                        <div className='text-center'>  <ClipLoader color={'#ED7117'} loading={registerLoading} size={30} aria-label="Loading Spinner" data-testid="loader" /></div>
+                        :
+                        <Button onClick={handleRegister} className='btn  mb-2'>Sign up</Button>}
+                    </div> :
+                      <div className="d-grid gap-2">
+                        <Button onClick={handleLogin} className='btn  mb-2'>Login</Button>
+                      </div>
+                    }
+                    <div className='row text-center'>
+                      {insideRegister && <div>
+                        <span>Or </span>
+                      </div>}
+                      {insideRegister && <div className="d-grid gap-2">
+                        <Button className='btn btn-light mb-2'> <i class="fa-brands fa-google me-1"></i>Continue with Google</Button>
+                      </div>}
+                      <Link to={'/home'}><i class="fa-solid fa-house fs-3"></i></Link>
                     </div>
-                  }
-                  <div className='row text-center'>
-                    {insideRegister && <div>
-                      <span>Or </span>
-                    </div>}
-                    {insideRegister && <div className="d-grid gap-2">
-                      <Button className='btn btn-light mb-2'> <i class="fa-brands fa-google me-1"></i>Continue with Google</Button>
-                    </div>}
-                    <Link to={'/home'}><i class="fa-solid fa-house fs-3"></i></Link>
-                  </div>
 
 
-                </Form>
+                  </Form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </div>}
       <ToastContainer theme='colored' />
     </div>
   )
